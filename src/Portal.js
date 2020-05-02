f697db8594935c96b9ca8e2bbb457c8c6ab90db3
maryam-algendy <maryamalgendy@gmail.com>
2020-05-02 21:07:25 +0200

    Update portal

--- src/Portal.js
+++ src/Portal.js
@@ -4,9 +4,9 @@ import MainNav from "components/MainNav";
 class Portal extends React.Component {
   render() {
     return (
-      <>
+      <div id="react-core" >
         <MainNav />
-      </>
+      </div>
     );
   }
 }
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,20 +1,22 @@
 import React from "react";
 
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 class MainNav extends React.Component{
     render()
     {
         return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="">React Core</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Button>Sign in</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <div className="navbar bg-light">
+                <div className='container '>
+                    <div className="logo">
+                        <a href="#">react core</a>
+                    </div>
+                    <div className="sign-in ml auto ">
+                        <Button className="primary">sign in</Button>
+                    </div>
+                </div>
+            </div>
+
         );
     }
 
