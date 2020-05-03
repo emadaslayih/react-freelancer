82950027179434b3fdfe5eca630c02838795742c
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-05-03 02:16:07 +0200

    Update MainNav

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -6,20 +6,18 @@ class MainNav extends React.Component{
     render()
     {
         return (
-            <div className="navbar bg-light">
-                <div className='container '>
-                    <div className="logo">
-                        <a href="#">react core</a>
-                    </div>
-                    <div className="sign-in ml auto ">
-                        <Button className="primary">sign in</Button>
-                    </div>
-                </div>
-            </div>
-
+			<div className="container">
+				<div className="navbar bg-light">
+				<div className="logo">
+					<a href="#">react core</a>
+				</div>
+				<div className="sign-in ml-auto">
+					<Button>sign in</Button>
+				</div>
+			</div>
+		</div>
         );
     }
-
 };
 
 export default MainNav;
