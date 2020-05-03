6b5c89be02c508cfd4b64f7d54d46776b9f9d0a8
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-05-03 02:16:32 +0200

    Create Masthead Component

--- src/Portal.js
+++ src/Portal.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import MainNav from "components/MainNav";
+import Masthead from "./components/Masthead";
 class Portal extends React.Component {
   render() {
     return (
       <div id="react-core">
         <MainNav />
+        <Masthead />
       </div>
     );
   }
