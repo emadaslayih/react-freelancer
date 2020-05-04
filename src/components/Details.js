d1e03f5db0d0c511d6b5a9b5d175a35d37d333dc
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-05-04 22:47:40 +0200

    Update Details.js
    Convert Details.js to Dynamic

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,18 +3,26 @@ import React from "react";
 const Details =()=>{
 
     return(
-        <div className="container-fluid details p-0">
-            <div className="row">
-                <div className="col-lg-6 order-lg-2 img-side"></div>
-                <div className="col-lg-6 order-lg-1 details-side my-auto">
-                    <h2>Fully Responsive Design</h2>
-                    <p className="mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device,
-                        whether it's a phone, tablet, or desktop the page will behave responsively!</p>
-                </div>
-            </div>
-        </div>
+       <div className="container-fluid details">
+           {
+                [1, 2, 3].map(key => {
+                    let imgClass = "order-lg-1"
+                    if(key === 2 && imgClass === "order-lg-1") {imgClass = ""}
+                    return (
+                        <div key={key} className="row">
+                            <div className={"col-lg-6 img-side " + imgClass}></div>
+                            <div className="col-lg-6 details-side my-auto">
+                                <h2>Fully Responsive Design</h2>
+                                <p className="mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device,
+                                    whether it's a phone, tablet, or desktop the page will behave responsively!
+                                </p>
+                            </div>
+                        </div>
+                    )
+                })
+           }
+       </div>
     );
 }
-
 
 export default Details;
