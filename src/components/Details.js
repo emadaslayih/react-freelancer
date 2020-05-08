9aada88cbe0aa02ed502481c178aaf8561fa120a
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-05-08 03:15:42 +0200

    Update Details.js
    -Update dynamic of Details.js Component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,21 +1,26 @@
 import React from "react";
+import CardBGImage from "./style/assets/bg-details-1.jpg";
 
 const Details =()=>{
+
+    const items = [
+        {image: CardBGImage, title: "Main Text", description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"},
+        {image: CardBGImage, title: "Main Text", description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"},
+        {image: CardBGImage, title: "Main Text", description: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"},
+    ]
 
     return(
        <div className="container-fluid details">
            {
-                [1, 2, 3].map(key => {
+                items.map((item ,key) => {
                     let imgClass = "order-lg-1"
-                    if(key === 2 && imgClass === "order-lg-1") {imgClass = ""}
+                    if((key % 2 !== 0) && imgClass === "order-lg-1") {imgClass = ""}
                     return (
                         <div key={key} className="row">
-                            <div className={"col-lg-6 img-side " + imgClass}></div>
+                            <div className={"col-lg-6 img-side " + imgClass} style = {{backgroundImage: `url(${item.image})`}} />
                             <div className="col-lg-6 details-side my-auto">
-                                <h2>Fully Responsive Design</h2>
-                                <p className="mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device,
-                                    whether it's a phone, tablet, or desktop the page will behave responsively!
-                                </p>
+                                <h2>{item.title}</h2>
+                                <p className="mb-0">{item.description}</p>
                             </div>
                         </div>
                     )
