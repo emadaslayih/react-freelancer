6d53381b8b8383b29c9fb53c848856520d0d0416
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-05-10 12:35:17 +0200

    Create freelancer_site structure

--- src/Portal.js
+++ src/Portal.js
@@ -1,26 +1,5 @@
 import React from "react";
 
-import MainNav from "components/MainNav";
-import Masthead from "./components/Masthead";
-import Features from "./components/Features";
-import Details from "./components/Details";
-import Testimonial from "./components/Testimonial";
-import Footer from "./components/Footer";
-class Portal extends React.Component {
-
-
-  render() {
-    return (
-      <div id="react-core">
-        <MainNav />
-        <Masthead />
-        <Features/>
-        <Details/>
-        <Testimonial/>
-        <Footer/>
-      </div>
-    );
-  }
+export default (props) => {
+    return <h1>Portal</h1>
 }
-
-export default Portal;
