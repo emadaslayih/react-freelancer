1e158439a0257036e4c1153386e36ce3db259e4f
maryam-algendy <maryamalgendy@gmail.com>
2020-06-16 21:13:49 +0200

    Add Navbar Component

--- src/Portal.js
+++ src/Portal.js
@@ -1,5 +1,13 @@
 import React from "react";
+import {Router,Route,Switch} from 'react-router-dom';
+
+import Navbar from "./components/navbar.js";
 
 export default (props) => {
-    return <h1>Portal</h1>
+    return  (
+        <div className="container-fluid px-0">
+            <Navbar/>   
+        </div>
+    )   
+
 }
