bb717c3465e5c01c277cc32c79923a09afaf968c
Mohamed Ghazi <m.mahmoud@uplift.plus>
2020-06-17 15:22:01 +0200

    Update repo Structure

--- src/Portal.js
+++ src/Portal.js
@@ -1,13 +1,9 @@
 import React from "react";
-import {Router,Route,Switch} from 'react-router-dom';
-
-import Navbar from "./components/navbar.js";
 
 export default (props) => {
     return  (
-        <div className="container-fluid px-0">
-            <Navbar/>   
+        <div id="portal">
+        	Portal  
         </div>
     )   
-
 }
