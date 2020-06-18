7265a9747e79294393bcb75e04f5f109ead4b39d
maryam-algendy <maryamalgendy@gmail.com>
2020-06-18 08:40:44 +0200

    Add Carousel

--- src/Portal.js
+++ src/Portal.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Router,Route,Switch} from 'react-router-dom';
 
 import Navbar from "./components/navbar.js";
-
+import Carousel from "./components/carousel.js"
 export default (props) => {
     return  (
         <div className="container-fluid px-0">
-            <Navbar/>   
+            <Navbar/>
+            <Carousel/>   
+
         </div>
     )   
 
