b9287c90fa10ca1d1442bc5aa9e53d1a2ba3f6aa
maryam-algendy <maryamalgendy@gmail.com>
2020-06-20 08:56:09 +0200

    Add What's great about it section

--- src/Portal.js
+++ src/Portal.js
@@ -4,13 +4,15 @@ import Navbar from "./components/navbar.js";
 import Carousel from "./components/carousel.js"
 import AsUsedBy from "./components/AsUsedBy.js"
 import WorkDone from "./components/WorkDone.js"
+import Features from "./components/Features.js"
 export default (props) => {
     return  (
         <div className="container-fluid px-0" title="Hier Freelancer & Find Freelancer Jobs Online">
             <Navbar/>
             <Carousel/>
             <AsUsedBy/>
-            <WorkDone/>   
+            <WorkDone/> 
+            <Features/>  
         </div>
     )   
 
