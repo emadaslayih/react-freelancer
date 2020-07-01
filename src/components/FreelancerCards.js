a9b8420202669d28d7edb3487e98f2299ceeecb9
maryam-algendy <maryamalgendy@gmail.com>
2020-07-01 19:59:36 +0200

    update freelancer cards

--- src/components/FreelancerCards.js
+++ src/components/FreelancerCards.js
@@ -7,14 +7,16 @@ const cards=[
         span:"FREELANCER API",
         title: "43 million professionals on demand",
         desc:"Why hire people when you can simply integrate our talented cloud workforce instead?",
-        btn:"view documentation"
+        btn:"view documentation",
+        txt:"text-xl-right"
     },
     {
         img:"fc.jpg" ,
         span:"FREELANCER ENTERPRISE",
         title: "Company budget? Get more done for less",
         desc:"Use our workforce of 43 million to help your business achieve more.",
-        btn:"contact us"
+        btn:"contact us",
+        imgClass:"order-xl-2",
     }
 ]
 
@@ -24,17 +26,13 @@ const FreelancerCards =()=>{
             <div className="row">
                 {
                     cards.map((card,i)=>{
-                        let imgClass = "order-xl-2"
-                        let txt="text-xl-right"
-                        if(i === 0 && imgClass === "order-xl-2") {imgClass = "" }
-                        if(i === 1 && txt=== "text-xl-right") {txt = "" }
                         return(
                             <div key={i} className="col-md-6">
                                 <div className="row lhs">
-                                    <div className={"col-xl-6 "+ imgClass}>
+                                    <div className={"col-xl-6 "+ card.imgClass}>
                                         <Image className="w-100" src={card.img}/>
                                     </div>
-                                    <div className={"col-xl-6 details "+txt}>
+                                    <div className={"col-xl-6 details "+card.txt}>
                                         <span>{card.span}</span>
                                         <h3>{card.title}</h3>
                                         <p>{card.desc}</p>
